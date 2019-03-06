b01c0dae422ab70d26ffcc10eda3d63ede6d0297
Marta Niemiec <niemiec.marta@hotmail.com>
2019-03-06 17:52:26 +0100

    RESTful API

--- NodeJs plus ExpressJs/serverExpress.js
+++ NodeJs plus ExpressJs/serverExpress.js
@@ -62,7 +62,7 @@ app.listen(3000);
 
 
 
-
+/*
 //====================
 // POSTMAN and body-parser
 const express = require('express');
@@ -89,5 +89,57 @@ app.post('/profile', (req, res) => {
   res.send('Success'); 
 })  
 
+
+app.listen(3000);
+*/
+
+
+
+
+
+//====================
+// RESTful API
+  // GET:
+  //  - req.query (localhost:3000/?name=andrei&age=31)  / "?" = query
+  //  - req.body
+  //  - req.headers
+  //  - req.params
+
+/*
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); 
+
+// app.get('/', (req, res) => { 
+//   // console.log(req.query); 
+//   // console.log(req.body)
+//   // console.log(req.headers)
+// });  
+
+// app.get('/:id', (req, res) => {   // params = id
+//   // console.log(req.params) //{ id: '123' }
+// }); 
+
+app.get('/:id', (req, res) => { 
+  console.log(req.params)
+  res.status(404).send("not found");
+});  
+
+
+app.listen(3000);
+*/
+
+
+// STATIC
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public')); //NodeJsplusExpressJs/public
 
 app.listen(3000);
