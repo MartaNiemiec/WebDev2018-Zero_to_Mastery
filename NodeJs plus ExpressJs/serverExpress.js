56e0345581385722b8307a41d308758a5e22971f
Marta Niemiec <niemiec.marta@hotmail.com>
2019-03-06 17:12:03 +0100

    using Postman and body-parser

--- NodeJs plus ExpressJs/serverExpress.js
+++ NodeJs plus ExpressJs/serverExpress.js
@@ -33,7 +33,7 @@ app.listen(3000);
 
 
 
-
+/*
 //====================
 // Express Middleware
 const express = require('express');
@@ -50,6 +50,43 @@ app.use((req, res, next) => { // ".use()" is a generic Express middleware
 
 app.get('/', (req, res) => { 
   res.send("tested"); 
+})  
+
+
+app.listen(3000);
+*/
+
+
+
+
+
+
+
+
+//====================
+// POSTMAN and body-parser
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false})) //parse url-encoded
+app.use(bodyParser.json()); //parse json file
+app.get('/', (req, res) => { 
+  res.send("getting root"); 
+})  
+
+app.get('/profile', (req, res) => { 
+  res.send("getting profile"); 
+})  
+
+app.post('/profile', (req, res) => { 
+  console.log(req.body); // access to the posted information; to use it it's needed middleware eg. body-parser
+  // const user = {
+  //   name: 'sally',
+  //   hobby: 'soccer'
+  // }
+  res.send('Success'); 
 })  
 
 
